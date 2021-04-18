cb0603bcaa84eddc015786ce9f2042609557d9aa
nesezon <tilichenko@gmail.com>
2021-04-18 15:13:37 +0300

    Задание 4. WpfStopwatch. Ядро без секундомера

---
+++ WpfStopwatch/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WpfStopwatch {
+  public partial class MainWindow : Window, INotifyPropertyChanged {
+
+    // реализация INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName) {
+      if (PropertyChanged != null) {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+    }
+
+    // свойство зависиомости для связывания с TextBlock
+    private double clocktxt;
+    public double Clocktxt {
+      get { return clocktxt; }
+      set {
+        clocktxt = value;
+        OnPropertyChanged("Clocktxt");
+      }
+    }
+
+    public MainWindow() {
+      InitializeComponent();
+      DataContext = this;
+    }
+
+    private void Start_Click(object sender, RoutedEventArgs e) {
+      Clocktxt = 123;
+    }
+
+    private void Stop_Click(object sender, RoutedEventArgs e) {
+      Clocktxt = 12345;
+    }
+
+    private void Reset_Click(object sender, RoutedEventArgs e) {
+      Clocktxt = 123456789.7;
+    }
+  }
+}
